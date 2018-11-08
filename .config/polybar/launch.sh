bb3777b106a95f502a70f455ef2b3ca94a5bb811
Xetro <golob.matevz@gmail.com>
2018-11-08 18:45:54 +0100

    Change Polybar to autodetect monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,11 @@ killall polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+outputs="$(polybar -m | cut -d ':' -f 1)"
+outputs=(${outputs[@]})
+
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you have to change it here too.
-polybar bar1 &
-polybar bar &
-
+MONITOR=${outputs[0]} polybar bar &
+MONITOR=${outputs[1]} polybar bar1 &
 echo "Bars launched..."
